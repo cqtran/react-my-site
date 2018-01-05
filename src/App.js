3f79405a47aa22daa170d95ab2e7a719350e0233
cqtran <cqtran@ualberta.ca>
2018-01-04 21:30:18 -0800

    created home page
    also imported rect-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './pages/Home.jsx';
+// imports home page from pages folder for routing 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+            <div>
+                <Route exact path ="/" component={Home}>  </Route>
+            </div>
+        </Router>
     );
   }
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Welcome</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+                <p>Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!</p>
+            </div>
+        );
+    }
+}
+
+export default Home
