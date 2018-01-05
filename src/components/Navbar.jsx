4818753537d15ec6bcb52fdcc16b69ed5ac1f821
cqtran <cqtran@ualberta.ca>
2018-01-04 22:22:59 -0800

    added footer + nabber fixes and div containers

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,23 +5,22 @@ import './Navbar.css';
 class Navbar extends Component{
     render() {
         return (
-<nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-      <a className="navbar-brand" href="#">Top navbar</a>
+<nav className="navbar navbar-expand-md">
+    <div className="container">
+      <Link className="navbar-brand" to="/">Cameron's React Website</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+            </ul>
+          </div>
       </div>
     </nav>
         );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
 
 
 
@@ -13,6 +14,7 @@ class Home extends Component {
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
                     <p>Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!</p>
                 </div>
+                <Footer />
             </div>
         );
     }
